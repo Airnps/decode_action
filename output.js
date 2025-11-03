8efbb46292188aa5e51348f994b7458541b030ec
action <action@github.com>
2025-11-03 03:21:55 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,29 +1,8 @@
-//Mon Nov 03 2025 03:20:07 GMT+0000 (Coordinated Universal Time)
+//Mon Nov 03 2025 03:21:55 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function abc() {
-  var _0xe3ab21 = dc();
-  $.ajax({
-    "type": "GET",
-    "url": originPath + _0xe3ab21,
-    "timeout": 30000,
-    "success": function (_0x1b0a10) {
-      {
-        if (_0x1b0a10.code == 200) {
-          if (_0x1b0a10.data.code == 200) sessionStorage.setItem("ffb", _0x1b0a10.data.a), sessionStorage.setItem(STR, _0x1b0a10.data.str), getTask();else {
-            if (302 == _0x1b0a10.data.code) titleText2.text("请进入公众号进行文章阅读"), $("#myModal2").show(), timerTask2();else {
-              titleText2.text("任务失效了！请返回");
-              $("#myModal2").show();
-              timerTask2();
-            }
-          }
-        } else titleText2.text("访问出错了，请返回重试。"), $("#myModal2").show(), timerTask2();
-      }
-    },
-    "error": function (_0xca068c, _0x3e1f14) {
-      titleText2.text("请求超时了！返回重试");
-      $("#myModal2").show();
-      timerTask2();
-    }
-  });
+function initDate() {
+  sessionStorage.setItem(START_NUM, "0");
+  sessionStorage.setItem(TOKEN, getTempValue());
+  abc();
 }
